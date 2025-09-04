6b406d46e178ba36481aa91dfa5a66309d4f41e1
Danila Rocha <danila.rocha.gloria@gmail.com>
2025-09-04 11:52:04 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    // JSX é uma sintaxe que permite escrever HTML dentro do JavaScript
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da pagina"/>
+        </header>
+    )
+}
+
+export default Banner;
