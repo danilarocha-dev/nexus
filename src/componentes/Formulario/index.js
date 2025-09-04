19efd27dd35e9ee95f04f045549ca0b8a5da0a29
Danila Rocha <danila.rocha.gloria@gmail.com>
2025-09-04 13:26:50 -0300

    alteracao no formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -29,7 +29,7 @@ const Formulario = (props) => {
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
-        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <h2>Bem-vindo! Vamos cadastrar um novo membro.</h2>
         <CampoTexto
           obrigatorio={true}
           label="Nome"
